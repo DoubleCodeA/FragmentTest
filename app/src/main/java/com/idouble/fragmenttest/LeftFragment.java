abd9e1fbf06bcb6fa27381b3038135d6ea5b5bf9
DoubleCodeA <37053790+DoubleCodeA@users.noreply.github.com>
2018-03-14 17:27:08 +0800

    Initial commit

---
+++ app/src/main/java/com/idouble/fragmenttest/LeftFragment.java
@@ -0,0 +1,25 @@
+package com.idouble.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Administrator on 2018/3/14.
+ */
+
+public class LeftFragment extends Fragment {
+    private Button button;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate (R.layout.left_fragment,container,false);
+    return view;
+    }
+
+}
